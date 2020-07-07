f63964c272872f2804d72f59e1f0bd3867ec5b34
guokonghui <gkhdeyouxiang@163.com>
2020-07-07 10:34:54 +0800

    delate build

--- 01_Cpp/1_Demo/LcmDemo/lcm_talker_listener/talker/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dh/Documents/lcm_talker_listener/talker
-# Build directory: /home/dh/Documents/lcm_talker_listener/talker/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
